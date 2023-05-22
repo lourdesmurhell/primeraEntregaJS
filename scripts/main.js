ae100bda5fec29968eb037cf5d63b8b4d69ede24
Lourdes <lourdesmurhell@gmail.com>
2023-05-22 19:01:23 -0300

    Realizando modificaciones al calculo del simulador

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,34 @@
 function calcularMonto() {
   while (true) {
-    // Obtener el monto ingresado por el usuario
-    var monto = parseFloat(prompt("Ingrese el monto a depositar:"));
+    // Obtener el monto de dinero con el que se quiere simular el plazo fijo
+    let monto = parseFloat(prompt("Ingrese el monto a depositar:"));
 
     // Obtener la opción de tiempo del plazo fijo
-    var opcion = parseInt(prompt("Opciones de tiempo del plazo fijo:\n1. 30 días\n2. 60 días\n3. 90 días\n4. 180 días\nSeleccione una opción:"));
+    let opcion = parseInt(prompt("Opciones de tiempo del plazo fijo:\n1. 30 días\n2. 60 días\n3. 90 días\n4. 180 días\nSeleccione una opción:"));
 
-    // Calcular la tasa de interés
-    var tasaInteres = 0.97;
+    // Calcular la tasa de interés según la opción elegida, teniendo en cuanta una TNA del 97%
+    let tasaInteres = 0;
+    if (opcion === 1) {
+      tasaInteres = 0.0808; // Tasa de interés para 30 días
+    } else if (opcion === 2) {
+      tasaInteres = 0.1616; // Tasa de interés para 60 días
+    } else if (opcion === 3) {
+      tasaInteres = 0.2424; // Tasa de interés para 90 días
+    } else if (opcion === 4) {
+      tasaInteres = 0.4848; // Tasa de interés para 180 días
+    } else {
+      alert("Opción inválida.");
+      continue; // Volver al inicio del ciclo
+    }
 
-    // Calcular el monto final
-    var montoFinal = calcularMontoFinal(monto, opcion, tasaInteres);
+    // Calcular el monto final según la opción elegida
+    let montoFinal = calcularMontoFinal(monto, tasaInteres);
 
-    // Mostrar el resultado
-    alert("El monto final al finalizar el plazo fijo será: " + montoFinal);
+    // Mostrar el resultado del rendimiento segun las opciones ingresadas
+    alert("El rendimiento al finalizar el plazo fijo será: $" + montoFinal);
 
     // Preguntar al usuario si desea calcular nuevamente
-    var continuar = prompt("¿Desea calcular nuevamente? (S/N)").toUpperCase();
+    let continuar = prompt("¿Desea calcular nuevamente? (S/N)").toUpperCase();
 
     // Salir del ciclo si el usuario no desea continuar
     if (continuar !== "S") {
@@ -25,30 +37,9 @@ function calcularMonto() {
   }
 }
 
-function calcularMontoFinal(monto, opcion, tasaInteres) {
-  var montoFinal = monto;
-
-  // Calcular el tiempo en días según la opción elegida
-  var tiempoEnDias = 0;
-  if (opcion === 1) {
-    tiempoEnDias = 30;
-  } else if (opcion === 2) {
-    tiempoEnDias = 60;
-  } else if (opcion === 3) {
-    tiempoEnDias = 90;
-  } else if (opcion === 4) {
-    tiempoEnDias = 180;
-  } else {
-    alert("Opción inválida.");
-    return; // Salir de la función
-  }
-
+function calcularMontoFinal(monto, tasaInteres) {
   // Calcular el monto final
-  var i = 0;
-  while (i < tiempoEnDias) {
-    montoFinal *= tasaInteres;
-    i++;
-  }
-
+  let montoFinal = monto * tasaInteres;
   return montoFinal;
 }
+
